3aa8dacd5b4dd02ef8c66932ef088e0716075ca6
katouyamato <112790178+katouyamato@users.noreply.github.com>
2022-10-17 16:01:57 +0900

    props

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -5,5 +5,6 @@
 
     コンポーネントテスト１
 
-    <x-tests.card title="タイトル1" content="本文1" :message="$message"/>
+    <x-tests.card title="タイトル1" content="本文1" :message="$message" />
+    <x-tests.card title="タイトル2" />
 </x-tests.app>
