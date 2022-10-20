bdef61030a2cc1032b4219a6787c5bcbea3c4d5b
katouyamato <112790178+katouyamato@users.noreply.github.com>
2022-10-20 14:17:41 +0900

    サービスプロバイダのテスト

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,8 @@ Route::get('/component-test1', [ComponentTestController::class, 'showComponent1'
 
 Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
 
-Route::get('/servicecontainer', [LifeCycleTestController::class, 'showServiceContainerTest']);
+Route::get('/servicecontainertest', [LifeCycleTestController::class, 'showServiceContainerTest']);
+
+Route::get('/serviceprovidertest', [LifeCycleTestController::class, 'showServiceProviderTest']);
 
 require __DIR__.'/auth.php';
