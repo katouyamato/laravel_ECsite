2b2bb3b18637c15a7350c754e58507d9b5ef9f83
katouyamato <112790178+katouyamato@users.noreply.github.com>
2022-10-25 20:16:30 +0900

    マルチログイン機能実装

--- resources/views/ADMIN/auth/login.blade.php
+++ resources/views/ADMIN/auth/login.blade.php
@@ -1,5 +1,6 @@
 <x-guest-layout>
     <x-auth-card>
+        管理者用
         <x-slot name="logo">
             <a href="/">
                 <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
@@ -9,7 +10,7 @@
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
 
-        <form method="POST" action="{{ route('login') }}">
+        <form method="POST" action="{{ route('admin.login') }}">
             @csrf
 
             <!-- Email Address -->
@@ -42,8 +43,8 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                @if (Route::has('admin.password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('admin.password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
--- resources/views/OWNER/auth/login.blade.php
+++ resources/views/OWNER/auth/login.blade.php
@@ -1,5 +1,6 @@
 <x-guest-layout>
     <x-auth-card>
+        オーナー様
         <x-slot name="logo">
             <a href="/">
                 <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
@@ -9,7 +10,7 @@
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
 
-        <form method="POST" action="{{ route('login') }}">
+        <form method="POST" action="{{ route('owner.login') }}">
             @csrf
 
             <!-- Email Address -->
@@ -42,8 +43,8 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                @if (Route::has('owner.password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('owner.password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
